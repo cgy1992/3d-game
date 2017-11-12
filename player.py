00c8d1bb0ddd35bd94dc581b2f23275be5709ff8
dekamaru <me@dekamaru.conm>
2017-11-12 13:51:30 +0300

    Added jumping

--- player.py
+++ player.py
@@ -5,11 +5,24 @@ class Player:
         self.input = input
 
         self.speed = 0.05
+        self.velocity_y = 0
+        self.gravity = 0.005
+        self.grounded = False
 
     def update(self):
         self.update_movement()
+        self.camera.updown(self.velocity_y)
 
     def update_movement(self):
+
+        self.velocity_y -= self.gravity
+
+        if self.camera.m_pos.y <= 1:
+            self.grounded = True
+            self.velocity_y = 0
+            self.camera.m_pos.y = 1
+            self.camera.m_view.y = 1
+
         if self.input.LEFT_KEY_PRESSED:
             self.camera.strafe(-self.speed)
 
@@ -17,4 +30,10 @@ class Player:
             self.camera.strafe(self.speed)
 
         if self.input.SPACE_KEY_PRESSED:
-            self.camera.updown(0.05)
+            if self.grounded:
+                self.velocity_y = 0.1
+                self.grounded = False
+        else:
+            if self.velocity_y > 0.1:
+                self.velocity_y = 0.1
+
